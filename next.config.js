a76260aba17b1a395dfbb51b0c64070e14a19280
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2023-10-15 16:51:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // reactStrictMode: true,
+  target: 'server',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
