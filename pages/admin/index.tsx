20a06302af87b19224dcc5ad860c85768594f681
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2023-10-16 12:28:06 +0300

    Create index.tsx

---
+++ pages/admin/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+const AppPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/admin/dashboard');
+  }, []);
+
+  return <div></div>;
+};
+
+export default AppPage;
