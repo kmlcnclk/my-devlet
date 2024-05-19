18a714388581299f55871b1c9ad765af9e67fb91
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2024-05-19 10:09:17 +0300

    Adding  multiple language support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -7,6 +7,19 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <Script src="https://c0f4f41c-2f55-4863-921b-sdk-docs.github.io/cdn/metamask-sdk.js"></Script>
+          <div className="gtranslate_wrapper"></div>
+
+          <script
+            src="https://cdn.gtranslate.net/widgets/latest/float.js"
+            defer
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.gtranslateSettings = {"default_language":"en","native_language_names":true,"languages":["en","tr","ru","uk","ar","es","ko"],"wrapper_selector":".gtranslate_wrapper"}
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
